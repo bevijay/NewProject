13ce58573dfa1328f9d77813b2b3e35d328ef5b6
Betigiri Vijay, B2B-ENS-KAT-SFS (EXT) <Vijay.Betigiri@swisscom.com>
2020-02-15 01:55:36 +0100

    new comment

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>54f59847-0c71-466f-b09b-53b37adf983c</testSuiteGuid>
+   <testCaseLink>
+      <guid>edc8d63c-c1d0-458d-a898-157e9c91649f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
